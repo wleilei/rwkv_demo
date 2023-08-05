6d008e9b8d954280e6bdb5c4526acc6e2eb8bc1a
wleilei <3166181399@qq.com>
2023-08-05 08:25:07 +0800

    Update para.py

--- para.py
+++ para.py
@@ -12,4 +12,5 @@ for i in keys:
     a = [product := product * j for j in w[i].shape]
     sum_p += a[-1]
     print(f"神经网络层：{i}, 形状：{w[i].shape}, 参数量{a[-1]}")
-print(sum_p)
+print(f"模型总参数量：{sum_p/1000000000:.2f}B")
+print(f"模型参数内存：{sum_p*4/1024/1024/1024:.2f}GB")
